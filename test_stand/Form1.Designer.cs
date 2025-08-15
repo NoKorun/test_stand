a65e05b3058a7afea575b8cf0172989117c12343
Korun <smolenskostya01@gmail.com>
2025-08-15 15:45:40 +0500

    Совместил код в одну форму

--- test_stand/Form1.Designer.cs
+++ test_stand/Form1.Designer.cs
@@ -30,7 +30,11 @@
         {
             this.pid_input_button = new System.Windows.Forms.Button();
             this.pidBox = new System.Windows.Forms.TextBox();
-            this.testControl1 = new test_stand.TestControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stop_test_button = new System.Windows.Forms.Button();
+            this.test3 = new System.Windows.Forms.Button();
+            this.test2 = new System.Windows.Forms.Button();
+            this.test1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // pid_input_button
@@ -51,36 +55,83 @@
             this.pidBox.TabIndex = 2;
             this.pidBox.TextChanged += new System.EventHandler(this.pidBox_TextChanged);
             // 
-            // testControl1
+            // label1
             // 
-            this.testControl1.InputText = "";
-            this.testControl1.Location = new System.Drawing.Point(147, 36);
-            this.testControl1.Name = "testControl1";
-            this.testControl1.ProgramStatus = "AwaitingPid";
-            this.testControl1.Size = new System.Drawing.Size(567, 226);
-            this.testControl1.TabIndex = 0;
-            this.testControl1.Load += new System.EventHandler(this.testControl1_Load);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(322, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Ожидается ввод идентификатора изделия";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // stop_test_button
+            // 
+            this.stop_test_button.Location = new System.Drawing.Point(374, 219);
+            this.stop_test_button.Name = "stop_test_button";
+            this.stop_test_button.Size = new System.Drawing.Size(75, 23);
+            this.stop_test_button.TabIndex = 8;
+            this.stop_test_button.Text = "Остановка теста";
+            this.stop_test_button.UseVisualStyleBackColor = true;
+            this.stop_test_button.Click += new System.EventHandler(this.stop_test_button_Click);
+            // 
+            // test3
+            // 
+            this.test3.Location = new System.Drawing.Point(554, 169);
+            this.test3.Name = "test3";
+            this.test3.Size = new System.Drawing.Size(75, 23);
+            this.test3.TabIndex = 7;
+            this.test3.Text = "Тест3";
+            this.test3.UseVisualStyleBackColor = true;
+            this.test3.Click += new System.EventHandler(this.test3_Click);
+            // 
+            // test2
+            // 
+            this.test2.Location = new System.Drawing.Point(374, 169);
+            this.test2.Name = "test2";
+            this.test2.Size = new System.Drawing.Size(75, 23);
+            this.test2.TabIndex = 6;
+            this.test2.Text = "Тест2";
+            this.test2.UseVisualStyleBackColor = true;
+            this.test2.Click += new System.EventHandler(this.test2_Click);
+            // 
+            // test1
+            // 
+            this.test1.Location = new System.Drawing.Point(177, 169);
+            this.test1.Name = "test1";
+            this.test1.Size = new System.Drawing.Size(75, 23);
+            this.test1.TabIndex = 5;
+            this.test1.Text = "Тест1";
+            this.test1.UseVisualStyleBackColor = true;
+            this.test1.Click += new System.EventHandler(this.test1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stop_test_button);
+            this.Controls.Add(this.test3);
+            this.Controls.Add(this.test2);
+            this.Controls.Add(this.test1);
             this.Controls.Add(this.pidBox);
             this.Controls.Add(this.pid_input_button);
-            this.Controls.Add(this.testControl1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Программа тестирования";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private TestControl testControl1;
         private System.Windows.Forms.Button pid_input_button;
         private System.Windows.Forms.TextBox pidBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button stop_test_button;
+        private System.Windows.Forms.Button test3;
+        private System.Windows.Forms.Button test2;
+        private System.Windows.Forms.Button test1;
     }
 }
 
--- test_stand/Form1.cs
+++ test_stand/Form1.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
@@ -13,23 +16,74 @@ namespace test_stand
 {
     public partial class Form1 : Form
     {
-        string[] pids = { "1", "2", "3", "4"};
+        string[] pids = { "1", "2", "3", "4"}; // Номера известных изделий
+        public string productID { get; set; } = ""; // PID (номер оборудования) из PIDBox
+        Random rnd = new Random();
+        public string programStatus { get; set; } = "AwaitingPid";
+        private CancellationTokenSource _cts;
+        private readonly string resultsDirectory = Path.Combine(Directory.GetCurrentDirectory(),
+            "Results"); // Папка для сохранения результатов тестов
 
         public Form1()
         {
+            if (!Directory.Exists(resultsDirectory)) // Проверяем, существует ли папка Results
+            {
+                Directory.CreateDirectory(resultsDirectory);
+            }
+
             InitializeComponent();
-
-            //testControl1.Visible = false; // Initially hide the control
         }
 
-        private void testControl1_Load(object sender, EventArgs e)
+        // Запись результатов тестов в JSON
+        private async void saveToJson<T>(T result, string inputText, string resultsDirectory)
         {
+            string fileName = $"TestResult_{productID}_{DateTime.Now:yyyyMMdd_HHmmss}.json";
+            string filePath = Path.Combine(resultsDirectory, fileName);
 
+            string json = JsonConvert.SerializeObject(result, Formatting.Indented);
+            try
+            {
+                using (var sw = new StreamWriter(filePath, false, Encoding.UTF8))
+                {
+                    await sw.WriteAsync(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка записи результата: {ex.Message}");
+            }
         }
 
+        // проверкой статуса программы + вызовом метода тестирования
+        private async Task RunTestWithChecks(Func<CancellationToken, Task> testMethod)
+        {
+            if (programStatus == "Testing")
+            {
+                MessageBox.Show("Тест уже запущен, пожалуйста, дождитесь его завершения");
+                return;
+            }
+            if (programStatus == "AwaitingPid")
+            {
+                MessageBox.Show("Пожалуйста, введите идентификатор изделия");
+                return;
+            }
+            programStatus = "Testing";
+            label1.Text = "Идет тестирование";
+
+            _cts = new CancellationTokenSource();
+            try
+            {
+                await testMethod(_cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                MessageBox.Show("Тест отменен пользователем");
+            }
+        }
+        // Обработчик нажатия кнопки для ввода PID
         private void pid_input_button_Click(object sender, EventArgs e)
         {
-            if (testControl1.ProgramStatus == "Testing")
+            if (programStatus == "Testing")
             {
                 MessageBox.Show("Test is already running. Please wait for it to finish.");
                 return;
@@ -37,8 +91,9 @@ namespace test_stand
             if (pids.Contains(pidBox.Text))
             {
                 //testControl1.Visible = true; // Show the control if PID is valid
-                testControl1.InputText = pidBox.Text;
-                testControl1.ProgramStatus = "AwaitingTest";
+                productID = pidBox.Text;
+                programStatus = "AwaitingTest";
+                label1.Text = "Ожидается начало теста";
 
                 //pid_input_button.Visible = false; // Disable the button after valid input
                 //pidBox.Visible = false; // Hide the PID input box after valid input
@@ -51,6 +106,101 @@ namespace test_stand
         }
 
         private void pidBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void test1_Click(object sender, EventArgs e)
+        {
+            await RunTestWithChecks(RunTestOne);
+        }
+        private async Task RunTestOne(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            await Task.Delay(10000, token);
+
+            int frequency = rnd.Next(100, 2000);
+            int test_result = rnd.Next(10); // Случайный результат теста
+            bool isSuccess = test_result < 5;
+
+            // Передача для записи в JSON
+            var result = new
+            {
+                Status = isSuccess ? "Успешно" : "Failure",
+                ErrorNumber = isSuccess ? 0 : test_result,
+                FrequencyMHz = frequency
+            };
+            saveToJson(result, productID, resultsDirectory);
+            programStatus = "Awaiting test";
+            label1.Text = "Ожидается начало теста";
+            MessageBox.Show("Тест заврешен");
+        }
+
+        private async void test2_Click(object sender, EventArgs e)
+        {
+            await RunTestWithChecks(RunTestTwo);
+        }
+        private async Task RunTestTwo(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            await Task.Delay(15000, token);
+
+            double resistance = rnd.NextDouble() * 1000;
+            int current = rnd.Next(1, 16);
+            int test_result = rnd.Next(10); // Случайный результат теста
+            bool isSuccess = test_result < 5;
+
+            // Передача для записи в JSON
+            var result = new
+            {
+                Status = isSuccess ? "Успешно" : "Failure",
+                ErrorNumber = isSuccess ? 0 : test_result,
+                ResistanceOm = resistance,
+                CurrentAmps = current
+            };
+            saveToJson(result, productID, resultsDirectory);
+            programStatus = "Awaiting test";
+            label1.Text = "Ожидается начало теста";
+            MessageBox.Show("Тест заврешен");
+        }
+
+        private async void test3_Click(object sender, EventArgs e)
+        {
+            await RunTestWithChecks(RunTestThree);
+        }
+        private async Task RunTestThree(CancellationToken token)
+        {
+            token.ThrowIfCancellationRequested();
+            await Task.Delay(20000, token);
+
+            int pressure = rnd.Next(10, 2000);
+            int test_result = rnd.Next(10); // Случайный результат теста
+            bool isSuccess = test_result < 5;
+
+            // Передача для записи в JSON
+            var result = new
+            {
+                Status = isSuccess ? "Успешно" : "Failure",
+                ErrorNumber = isSuccess ? 0 : test_result,
+                PressurePa = pressure
+            };
+            saveToJson(result, productID, resultsDirectory);
+            programStatus = "Awaiting test";
+            label1.Text = "Ожидается начало теста";
+            MessageBox.Show("Тест заврешен");
+        }
+
+        private void stop_test_button_Click(object sender, EventArgs e)
+        {
+            if (programStatus == "Testing")
+            {
+                _cts?.Cancel();
+                programStatus = "AwaitingTest";
+                label1.Text = "Ожидается начало теста";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
